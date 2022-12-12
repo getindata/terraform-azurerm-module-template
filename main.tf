ef2b0f0df6e074e8e8cc708492bf8129ca001149
Jakub Igla <jakub.igla@getindata.com>
2022-12-12 09:08:33 +0100

    feat: Azure Terraform template files

--- main.tf
+++ main.tf
@@ -1,13 +1,26 @@
-# Example resource that outputs the input value and 
-# echoes it's base64 encoded version locally 
+data "azurerm_resource_group" "this" {
+  count = module.this.enabled && var.location == null ? 1 : 0
 
-resource "null_resource" "output_input" {
-  triggers = {
-    name  = local.name_from_descriptor
-    input = var.example_var
-  }
+  name = var.resource_group_name
+}
 
-  provisioner "local-exec" {
-    command = "echo ${var.example_var} | base64"
-  }
+#This is a sample resource
+resource "azurerm_public_ip" "this" {
+  count = module.this.enabled ? 1 : 0
+
+  name                = local.name_from_descriptor
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  allocation_method = "Dynamic"
+}
+
+module "diagnostic_settings" {
+  count = module.this.enabled && var.diagnostic_settings.enabled != null ? 1 : 0
+
+  source  = "claranet/diagnostic-settings/azurerm"
+  version = "6.2.0"
+
+  resource_id           = one(azurerm_public_ip.this[*].id)
+  logs_destinations_ids = var.diagnostic_settings.logs_destinations_ids
 }
